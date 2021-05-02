5325158a846f64d24bd7c56eb1bc53d4862632fc
Simon Barnett <simonbarnett@emerge-it.co.uk>
2021-05-02 20:17:53 +0100

    fixes

---
+++ frmLog.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLog
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLog))
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SearchText = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 4
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.DateTimePicker1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.SearchText, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 3, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(482, 277)
+        Me.TableLayoutPanel1.TabIndex = 1
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DateTimePicker1.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(3, 0)
+        Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(3, 0, 3, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(194, 24)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.Black
+        Me.TableLayoutPanel1.SetColumnSpan(Me.RichTextBox1, 4)
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.Lime
+        Me.RichTextBox1.Location = New System.Drawing.Point(3, 24)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 0, 3, 3)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.TableLayoutPanel1.SetRowSpan(Me.RichTextBox1, 4)
+        Me.RichTextBox1.ShowSelectionMargin = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(476, 250)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.WordWrap = False
+        '
+        'SearchText
+        '
+        Me.SearchText.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SearchText.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SearchText.Location = New System.Drawing.Point(325, 0)
+        Me.SearchText.Margin = New System.Windows.Forms.Padding(3, 0, 0, 3)
+        Me.SearchText.Name = "SearchText"
+        Me.SearchText.Size = New System.Drawing.Size(117, 24)
+        Me.SearchText.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(442, 0)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(40, 24)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'frmLog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "frmLog"
+        Me.Size = New System.Drawing.Size(482, 277)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As Windows.Forms.TableLayoutPanel
+    Friend WithEvents DateTimePicker1 As Windows.Forms.DateTimePicker
+    Friend WithEvents RichTextBox1 As Windows.Forms.RichTextBox
+    Friend WithEvents SearchText As Windows.Forms.TextBox
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents PictureBox1 As Windows.Forms.PictureBox
+End Class
---
+++ frmLog.vb
@@ -0,0 +1,242 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Public Class frmLog
+
+    Private _Root As DirectoryInfo = Nothing
+    Public Property Root As DirectoryInfo
+        Get
+            Return _Root
+        End Get
+        Set(value As DirectoryInfo)
+            _Root = value
+        End Set
+    End Property
+    Private Sub frmLog_Load(sender As Object, e As EventArgs) Handles Me.Load
+        With RichTextBox1
+            .Focus()
+
+        End With
+    End Sub
+
+#Region "Date/Folder properties"
+
+    Public Property [Date] As Date
+        Get
+            Return DateTimePicker1.Value
+        End Get
+        Set(value As Date)
+            DateTimePicker1.Value = value
+        End Set
+    End Property
+
+    Private ReadOnly Property Folder As DirectoryInfo
+        Get
+            With DateTimePicker1.Value
+                Return New DirectoryInfo(
+                    Path.Combine(
+                       _Root.FullName,
+                        .ToString("yyyy-MM")
+                    )
+                )
+            End With
+        End Get
+    End Property
+
+    Private ReadOnly Property Filename As FileInfo
+        Get
+            With DateTimePicker1.Value
+                Return New FileInfo(
+                    Path.Combine(
+                        Folder.FullName,
+                        String.Format(
+                            "{0}.txt",
+                            .ToString("yyMMdd")
+                        )
+                    )
+                )
+            End With
+        End Get
+
+    End Property
+
+#End Region
+
+#Region "File Watcher"
+
+    Private Sub FileSystemWatcher1_Changed(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher1.Changed
+
+        If String.Compare(e.FullPath, Filename.FullName, True) = 0 Then
+
+            Dim l As Integer = 0
+            Dim f As Boolean = False
+
+            Do
+                Try
+                    Using SR As New StreamReader(Filename.FullName)
+                        While Not SR.EndOfStream
+                            Dim str = SR.ReadLine
+                            With RichTextBox1
+                                If l >= UBound(.Lines) Then
+                                    .AppendText(str & Environment.NewLine)
+                                    .SelectionStart = .Text.Length
+                                    .ScrollToCaret()
+
+                                End If
+                            End With
+
+                            l += 1
+                        End While
+
+                    End Using
+                    f = True
+
+                Catch ex As Exception
+                    Threading.Thread.Sleep(100)
+                    f = False
+
+                End Try
+
+            Loop Until f
+
+        End If
+
+    End Sub
+
+    Private Sub FileSystemWatcher1_Created(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher1.Created
+
+        With RichTextBox1
+            If String.Compare(e.FullPath, Filename.FullName, True) = 0 Then
+                readFile()
+                .SelectionStart = .Text.Length
+                .ScrollToCaret()
+
+            End If
+        End With
+
+    End Sub
+
+#End Region
+
+#Region "Controls"
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+        If Not Root Is Nothing Then
+
+            With FileSystemWatcher1
+                .EnableRaisingEvents = True
+                .Path = Filename.Directory.FullName
+                .IncludeSubdirectories = False
+            End With
+
+            With RichTextBox1
+                If Filename.Exists Then
+                    Dim l As Integer = 0
+                    readFile()
+
+                Else
+                    .Clear()
+
+                End If
+
+                .SelectionStart = .Text.Length
+                .ScrollToCaret()
+                .Focus()
+
+            End With
+
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Search"
+
+    Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        If SearchText.Text.Length > 0 Then
+
+            With RichTextBox1
+
+                Dim ind As Integer
+                Dim s As Integer = .SelectionStart
+                Dim l As Integer = .SelectionLength
+
+                .SelectionStart = .SelectionStart + .SelectionLength
+                Dim finds As New RichTextBoxFinds
+                ind = .Find(SearchText.Text, .SelectionStart, finds)
+
+                If ind >= 0 Then
+                    .SelectionStart = ind
+                    .SelectionLength = SearchText.Text.Length
+
+                Else
+                    .SelectionStart = .Text.Length
+                    .SelectionLength = 0
+
+                End If
+
+                .ScrollToCaret()
+                .Focus()
+
+            End With
+
+        End If
+
+    End Sub
+
+    Private Sub RichTextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles RichTextBox1.KeyUp, Me.KeyUp, DateTimePicker1.KeyUp
+        If e.KeyData = Keys.F Or e.Control Then
+            SearchText.Focus()
+
+        ElseIf e.KeyData = Keys.F3 Then
+            btnFind_Click(Me, New EventArgs)
+
+        End If
+
+    End Sub
+
+    Private Sub SearchText_KeyDown(sender As Object, e As KeyEventArgs) Handles SearchText.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                btnFind_Click(Me, New EventArgs)
+
+        End Select
+
+    End Sub
+
+#End Region
+
+    Sub readFile()
+
+        Dim f As Boolean = False
+        With RichTextBox1
+            Do
+                Try
+                    Using SR As New StreamReader(Filename.FullName)
+                        .Text = SR.ReadToEnd
+
+                    End Using
+                    f = False
+
+                Catch ex As Exception
+                    Threading.Thread.Sleep(100)
+                    f = True
+
+                End Try
+            Loop Until Not f
+
+        End With
+
+    End Sub
+
+    Private Sub frmLog_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        With Me.RichTextBox1
+            .Width = (Me.Width - 3)
+            .Height = Me.Height - 40
+        End With
+
+    End Sub
+End Class
